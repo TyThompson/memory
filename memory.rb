14bc3dc662938f77183e2f3e6e6d1e053234f48d
tythompson <tythompson@gmail.com>
2016-05-20 11:50:50 -0400

    Added more methods, game is working, looking at adding more features

--- memory.rb
+++ memory.rb
@@ -15,9 +15,38 @@ require "pry"
 	def displaycard(board, checkcard)
 		puts board[checkcard]
 	end
-	
+
+	def cardmatch(board, checkcard)
+		puts
+		print "You picked a match, #{board[checkcard]}"
+		puts
+		puts
+	end
+
+	def cardpicked(board, checkcard)
+		puts "The card you picked was, #{board[checkcard]}"
+	end
+
+	def cardpicked2(board2, checkcard2)
+		puts
+		print "The card you picked is #{board2[checkcard2]} and is not a match."
+		puts
+	end
+
+	def showmatched(matched)
+			print matched
+			puts
+	end
+
 	def gameovercheck?(matched)
 		matched == board
+	end
+
+	def quitcheck?(i)
+		if i == "quit"
+		print "Ending game because a player quit."
+		return true
+	  end
 	end
 
 
@@ -28,7 +57,8 @@ require "pry"
 	board2 = []
 	matched = []
 	cardsymbols = ["!", "@", "#", "$", "%", "&", "+", "?"]
-	checkcard =nil
+	checkcard = "nothing"
+	checkcard2 = "nothing"
 
 	#board setup
 	print "How many cards would you like to use?    "
@@ -41,23 +71,22 @@ require "pry"
 
 
 	#game loop
-	loop do
-		print "Which card would you like to reveal?   "
-		checkcard = gets.chomp.to_i - 1
-		puts "The card you picked was, #{board[checkcard]}"
-		puts
-		print "Pick card from 2nd set that matches the symbol?   "
-		checkcard2 = gets.chomp.to_i - 1
-		
+	until quitcheck?(checkcard) do
+		print "Which card would you like to reveal? (1 - #{boardsize})  "
+		checkcard = gets.chomp
+		quitcheck? checkcard
+		checkcard = checkcard.to_i - 1
+		cardpicked(board, checkcard)
+		print "Pick card from 2nd set that matches the symbol? (1 - #{boardsize})   "
+		checkcard2 = gets.chomp
+		quitcheck? checkcard2
+		checkcard2 = checkcard2.to_i - 1
+
 		if board[checkcard] == board2[checkcard2]
-		puts
-		print "You picked a match, #{board[checkcard]}"
-		puts
+		cardmatch(board, checkcard)
 		matched << board[checkcard]
+		showmatched(matched)
 			else
-		puts
-		print "The card you picked is #{board2[checkcard2]} and is not a match."
-		puts
+		cardpicked2(board2,checkcard2)
 			end
-			
 	end
